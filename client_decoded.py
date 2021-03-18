3003ed203bfeb3590656db6fce01b97d9cebb8ec
mynameisteodora <gteodoramaria@gmail.com>
2021-03-18 10:32:19 +0000

    server triggering nrfutil command

--- client_decoded.py
+++ client_decoded.py
@@ -10,36 +10,80 @@ import pty
 # ser = serial.Serial(s_name)
 # print(s_name)
 
+class Slip:
+    SLIP_BYTE_END             = 0o300
+    SLIP_BYTE_ESC             = 0o333
+    SLIP_BYTE_ESC_END         = 0o334
+    SLIP_BYTE_ESC_ESC         = 0o335
+
+    SLIP_STATE_DECODING                 = 1
+    SLIP_STATE_ESC_RECEIVED             = 2
+    SLIP_STATE_CLEARING_INVALID_PACKET  = 3
+
+    @staticmethod
+    def encode(data):
+        newData = []
+        for elem in data:
+            if elem == Slip.SLIP_BYTE_END:
+                newData.append(Slip.SLIP_BYTE_ESC)
+                newData.append(Slip.SLIP_BYTE_ESC_END)
+            elif elem == Slip.SLIP_BYTE_ESC:
+                newData.append(Slip.SLIP_BYTE_ESC)
+                newData.append(Slip.SLIP_BYTE_ESC_ESC)
+            else:
+                newData.append(elem)
+        newData.append(Slip.SLIP_BYTE_END)
+        return newData
+
+    @staticmethod
+    def decode_add_byte(c, decoded_data, current_state):
+        finished = False
+        if current_state == Slip.SLIP_STATE_DECODING:
+            if c == Slip.SLIP_BYTE_END:
+                finished = True
+            elif c == Slip.SLIP_BYTE_ESC:
+                current_state = Slip.SLIP_STATE_ESC_RECEIVED
+            else:
+                decoded_data.append(c)
+        elif current_state == Slip.SLIP_STATE_ESC_RECEIVED:
+            if c == Slip.SLIP_BYTE_ESC_END:
+                decoded_data.append(Slip.SLIP_BYTE_END)
+                current_state = Slip.SLIP_STATE_DECODING
+            elif c == Slip.SLIP_BYTE_ESC_ESC:
+                decoded_data.append(Slip.SLIP_BYTE_ESC)
+                current_state = Slip.SLIP_STATE_DECODING
+            else:
+                current_state = Slip.SLIP_STATE_CLEARING_INVALID_PACKET
+        elif current_state == Slip.SLIP_STATE_CLEARING_INVALID_PACKET:
+            if c == Slip.SLIP_BYTE_END:
+                current_state = Slip.SLIP_STATE_DECODING
+                decoded_data = []
+
+        return (finished, current_state, decoded_data)
+
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ("35.246.27.70", 5002)
+server_address = ("127.0.0.1", 15200)
 print(f'connecting to {server_address[0]} port {server_address[1]}')
 sock.connect(server_address)
 
 user_message = ""
 
-while True and user_message != "Bye":
-
-    user_message = input("Enter a message to send\n")
-
-    # ser.write(user_message.encode('utf-8'))
+while True:
 
     try:
+        data = sock.recv(50000)
+        print("Received data = ", data)
 
-        # Send data
-        # message = os.read(master, 1000)
-        print(f'sending "{user_message}"')
-        sock.sendall(user_message.encode('utf-8'))
+        if data:
+            resp = [0x60, 0x09, 0x01]
+            encoded = Slip.encode(resp)
+            print("sending encoded data: ", encoded)
+            print("of type", type(encoded))
+            sock.send(bytearray(Slip.encode(resp)))
 
-        data = sock.recv(1000)
-        print(f'received "{data}"')
-
-        if data.decode('utf-8').strip() == 'Bye':
-            print("Received bye from server, closing connection")
-            sock.close()
-            break
 
     except Exception as e:
         print('closing socket because of error')
--- echo_server.py
+++ echo_server.py
@@ -1,11 +1,23 @@
 import socket
-import sys
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description='Script so useful.')
+parser.add_argument("--file", type=str, default="app_update.zip")
+parser.add_argument("--port", type=str)
+parser.add_argument("--baud_rate", type=int, default=9600)
+
+args = parser.parse_args()
+
+zip_update = args.file
+port = args.port
+baud_rate = args.baud_rate
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('0.0.0.0', 5002)
+server_address = ('', 5002)
 print(f'starting up on {server_address[0]} port {server_address[1]}')
 sock.bind(server_address)
 
@@ -23,11 +35,12 @@ while True:
         # Receive the data in small chunks and retransmit it
         while True:
             print('Waiting for message...')
-            data = connection.recv(1000)
-            print('received "%s"' % data)
+            data = connection.recv(50000)
+            print('received "%s"' % data) # this should be any data from airspeck and we need to receive it in one batch (messageLength = 117?)
             if data:
-                reply = input('Send something back:')
-                connection.sendall(reply.encode('utf-8'))
+                reply = input('Send nrf components back:')
+                subprocess.run((f"nrfutil dfu serial -pkg {zip_update} -p {port} -b {baud_rate} -fc 0").split())
+                # connection.sendall(reply.encode('utf-8'))
             else:
                 print('no more data from', client_address)
                 break
